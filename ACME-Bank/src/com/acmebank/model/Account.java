150821bfbca683b77315f0a4de36413170c0a58e
Tiger-Catori <michaeladeleye365@gmail.com>
2026-04-03 21:42:34 +0100

    Defing common fields and methods

--- ACME-Bank/src/com/acmebank/model/Account.java
+++ ACME-Bank/src/com/acmebank/model/Account.java
@@ -1,4 +1,24 @@
 package com.acmebank.model;
 
 public abstract class Account {
+    // The blueprint for all bank accounts.
+    // No real account of this type will exist.
+    // Just defines common fields and methods.
+
+    // Variables
+    private int accountNumber;
+    private int sortCode;
+    private double currentBalance;
+
+
+    // Abstract Methods
+    public abstract double deposit();
+    public abstract double withdraw();
+    public abstract double calculateInterest();
+
+    // Concrete Methods
+//    public double getBalance() {}
+//    public int getAccountNumber() {}
+//    public int getSortCode() {}
+//    public void displayDetails(){}
 }
