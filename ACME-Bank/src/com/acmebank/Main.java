52ef966e0fd9a0f82fa6d06b03be256c912b1d2a
Tiger-Catori <michaeladeleye365@gmail.com>
2026-04-05 15:31:13 +0100

    Implementing Constructors and Methods for PersonalAccount subclass

--- ACME-Bank/src/com/acmebank/Main.java
+++ ACME-Bank/src/com/acmebank/Main.java
@@ -1,9 +1,16 @@
 package com.acmebank;
 
+import com.acmebank.model.Account;
+import com.acmebank.model.PersonalAccount;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Account account1 = new PersonalAccount(10.348);
+        Account account2 = new PersonalAccount(89.139);
+        account1.displayDetails();
+        System.out.println();
+        account2.displayDetails();
     }
 
 }
--- ACME-Bank/src/com/acmebank/model/Customer.java
+++ ACME-Bank/src/com/acmebank/model/Customer.java
@@ -1,5 +1,25 @@
 package com.acmebank.model;
 
-public class Customer {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Customer {
+    private int customerID;
+    private String firstName;
+    private String lastName;
+
+    public Customer(int customerID, String firstName, String lastName) {
+        this.customerID = customerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    List<Account> accountList = new ArrayList<>();
+//    accountList.add(new PersonalAccount());
+//    accountList.add(new BusinessAccount());
+//    accountList.add(new IsaAccount());
+
+//    public void addNewAccount() {
+//        if (accountList.contains(typeof(IsaAccount)))
+//    }
 }
