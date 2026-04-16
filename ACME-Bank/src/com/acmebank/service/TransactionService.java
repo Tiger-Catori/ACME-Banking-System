2fbfcee69f579657f8245efdef301c0285a0ce3b
Tiger-Catori <michaeladeleye365@gmail.com>
2026-04-16 23:29:57 +0100

    Completing transactionservice

--- ACME-Bank/src/com/acmebank/service/TransactionService.java
+++ ACME-Bank/src/com/acmebank/service/TransactionService.java
@@ -1,5 +1,6 @@
 package com.acmebank.service;
 
+import com.acmebank.exceptions.InvalidAmountException;
 import com.acmebank.infrastructure.logging.AuditLogger;
 import com.acmebank.infrastructure.persistance.DataPersistance;
 import com.acmebank.model.Account;
@@ -26,31 +27,78 @@ public class TransactionService {
     }
 
     public void deposit(Account account, double amount) {
-        account.deposit(amount);
-        double newBalance = account.getBalance();
-        // Creating transaction object
-        Transaction transaction = Transaction.create(LocalDateTime.now(), TransactionType.DEPOSIT,"Cash Deposit", amount, newBalance);
-        account.addTransaction(transaction);
-        // Logging event
-        auditLogger.log("Deposited £" + amount + " into account " + account.getAccountNumber());
-        // Saving all customers.
-        dataPersistance.saveCustomers(allCustomers);
+        // Validating amount
+        if (amount <= 0) {
+            account.deposit(amount);
+            double newBalance = account.getBalance();
+            // Creating transaction object
+            Transaction transaction = Transaction.create(
+                    LocalDateTime.now(),
+                    TransactionType.DEPOSIT,
+                    "Cash Deposit",
+                    amount, newBalance);
+            account.addTransaction(transaction);
+            // Logging event
+            auditLogger.log("Deposited £" + amount + " into account " + account.getAccountNumber());
+            // Saving all customers.
+            dataPersistance.saveCustomers(allCustomers);
+        } else {
+            throw new InvalidAmountException("Deposit amount must be positive");
+        }
 
     }
 
     public void withdraw(Account account, double amount) {
-        account.withdraw(amount);
-        double newBalance = account.getBalance();
-        // Creating transaction object
-        Transaction transaction = Transaction.create(LocalDateTime.now(), TransactionType.WITHDRAWAL, "Cash Withdrawal", amount, newBalance);
-        account.addTransaction(transaction);
-        // Logging event
-        auditLogger.log("Withdrew £" + amount + " into account " + account.getAccountNumber());
-        // Saving all customers
-        dataPersistance.saveCustomers(allCustomers);
+        // Validating amount
+        if (amount > 0) {
+            account.withdraw(amount);
+            double newBalance = account.getBalance();
+            // Creating transaction object
+            Transaction transaction = Transaction.create(
+                    LocalDateTime.now(),
+                    TransactionType.WITHDRAWAL,
+                    "Cash Withdrawal",
+                    amount, newBalance);
+            account.addTransaction(transaction);
+            // Logging event
+            auditLogger.log("Withdrew £" + amount + " into account " + account.getAccountNumber());
+            // Saving all customers
+            dataPersistance.saveCustomers(allCustomers);
+        } else {
+            throw new InvalidAmountException("Withdrawal amount must be positive");
+        }
+
     }
 
-    public void transfer (Account fromAccount, Account toAccount, double amount) {
+    public void transfer(Account fromAccount, Account toAccount, double amount) {
+        // Validation
+        if (amount > 0 && (fromAccount.getAccountNumber() != toAccount.getAccountNumber())) {
+            fromAccount.withdraw(amount);
+            toAccount.deposit(amount);
+            // Creating transaction for source account
+            Transaction sourceTransaction = Transaction.create(
+                    LocalDateTime.now(),
+                    TransactionType.TRANSFER,
+                    "Transfer to account " + toAccount.getAccountNumber(),
+                    amount, fromAccount.getBalance()
+            );
+            // Creating transaction for destination account
+            Transaction destinationTransaction = Transaction.create(
+                    LocalDateTime.now(),
+                    TransactionType.TRANSFER,
+                    "Transfer from account " + fromAccount.getAccountNumber(),
+                    amount, toAccount.getBalance()
+            );
+            // Adding transactions
+            toAccount.addTransaction(destinationTransaction);
+            fromAccount.addTransaction(sourceTransaction);
+            // Logging it
+            auditLogger.log("Transferred £" + amount + " from account " +
+                    fromAccount.getAccountNumber() + " to account " +
+                    toAccount.getAccountNumber());
+            // Saving customers
+            dataPersistance.saveCustomers(allCustomers);
+        }
 
     }
 }
