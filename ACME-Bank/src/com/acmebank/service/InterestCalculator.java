cf80518fbf7615be665ef80cf2dbb58277f9c716
Tiger-Catori <michaeladeleye365@gmail.com>
2026-04-17 16:28:06 +0100

    interest calculator

--- ACME-Bank/src/com/acmebank/service/InterestCalculator.java
+++ ACME-Bank/src/com/acmebank/service/InterestCalculator.java
@@ -1,5 +1,19 @@
 package com.acmebank.service;
 
+import com.acmebank.model.Account;
+import com.acmebank.model.IsaAccount;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class InterestCalculator {
 
+    private final static double INTEREST_RATE = 0.0275;
+
+    public BigDecimal calculateInterest(IsaAccount account) {
+        BigDecimal balance = BigDecimal.valueOf(account.getBalance());
+        BigDecimal rate = BigDecimal.valueOf(INTEREST_RATE);
+
+        return balance.multiply(rate).setScale(2, RoundingMode.HALF_UP);
+    }
 }
